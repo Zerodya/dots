c5c53197d89613a3d452f4f5178076564d14ce64
TheAlphaCeph <73220426+TheAlphaCeph@users.noreply.github.com>
2022-05-25 16:47:33 +0000

    Add files via upload

--- .config/polybar/scripts/spotify_status.py
+++ .config/polybar/scripts/spotify_status.py
@@ -68,7 +68,7 @@ def truncate(name, trunclen):
 
 # Default parameters
 output = fix_string(u'{play_pause} {artist}: {song}')
-trunclen = 35
+trunclen = 40
 play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
 
 label_with_font = '%{{T{font}}}{label}%{{T-}}'
